2297db5d0eeb107d75a8b8d0d78b90fe3b230b2d
joseph tharwat <jo.tharwat.jt@gmail.com>
2025-09-18 15:04:21 +0300

    change Grading service to be api project

---
+++ GradingManagment/Infrastructure/RabbitMQ/GetTestInfoWorker.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GradingManagment.Infrastructure.RabbitMQ
+{
+    public class GetTestInfoWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            
+            return Task.CompletedTask;
+        }
+    }
+}
