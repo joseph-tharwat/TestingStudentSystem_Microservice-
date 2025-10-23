0dbe5fd082b390df7dd419a42d570f1df7465e30
joseph tharwat <jo.tharwat.jt@gmail.com>
2025-10-23 14:48:22 +0300

    add shared project to the repo

---
+++ SharedLogger/SerilogSeqConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace SharedLogger
+{
+    public static class SerilogSeqConfiguration
+    {
+        public static void SerilogSeqConfigur(string serviceName, IConfiguration config)
+        {
+            var seqUrl = config.GetSection("Seq:Url").Value ?? "http://localhost:5341";
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Service", serviceName)
+                .Enrich.FromLogContext()
+                .WriteTo.Seq(seqUrl)
+                .CreateLogger();
+        }
+    }
+}
